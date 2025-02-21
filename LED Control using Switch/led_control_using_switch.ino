d91647feb6715505f3ac8d27007b3eb2e036184c
MAYILRAJ R <163078420+Blackboy13@users.noreply.github.com>
2025-02-21 22:16:47 +0530

    Update led_control_using_switch.ino

--- LED Control using Switch/led_control_using_switch.ino
+++ LED Control using Switch/led_control_using_switch.ino
@@ -1,14 +1,14 @@
 int button = 7;
 void setup()
 {
-    // put your setup code here, to run once:
-    pinMode(LED_BUILTIN, OUTPUT); //to initialise built-in LED pin 13 as output
-    pinMode(button, INPUT);       //to initialize pin 7 as input of pushbutton switch
+    
+    pinMode(LED_BUILTIN, OUTPUT);
+    pinMode(button, INPUT);       
 }
 
 void loop()
 {
-    // put your main code here, to run repeatedly:
+   
     if (digitalRead(button) == HIGH)
         digitalWrite(LED_BUILTIN, HIGH);
     else
