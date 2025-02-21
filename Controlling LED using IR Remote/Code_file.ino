8ff3b19e9c3b1ea5c5bc1d7c190625abfc90c70f
MAYILRAJ R <163078420+Blackboy13@users.noreply.github.com>
2025-02-21 22:15:47 +0530

    Update Code_file.ino

--- Controlling LED using IR Remote/Code_file.ino
+++ Controlling LED using IR Remote/Code_file.ino
@@ -1,38 +1,38 @@
-#include <IRremote.h>    //adding library for IR sensor
-int led = 5;             // Setting led to pin 5
-int RECV_PIN = 6;        // Pin of IR sensor
-int glow = 0;            // Declaring a variable to detect whether led is turned on/off
-IRrecv irrecv(RECV_PIN); // For led pin
-decode_results results;  // Variable foroutput from IR sensor
+#include <IRremote.h>    
+int led = 5;             
+int RECV_PIN = 6;        
+int glow = 0;            
+IRrecv irrecv(RECV_PIN); 
+decode_results results;  
 
 void setup()
 {
-    pinMode(led, OUTPUT); // Set LED pin
-    Serial.begin(9600);   // Begin the serial monitor
-    irrecv.enableIRIn();  // Begining the IR sensor
+    pinMode(led, OUTPUT); 
+    Serial.begin(9600);   
+    irrecv.enableIRIn();  
 }
 
 void loop()
 {
-    if (irrecv.decode(&results)) // If any pushing of buttons detected
+    if (irrecv.decode(&results)) 
     {
-        unsigned int value = results.value; // A variable to store output from IR sensor
-        Serial.println(value);              // Printing the output on serrial monitor
+        unsigned int value = results.value; 
+        Serial.println(value);             
 
         if (value == 255)
-        { // If Voltage value was detected as 255
+        { 
             if (glow == 0)
-            {                            // and LED was off
-                digitalWrite(led, HIGH); // Turn on the LED
-                glow = 1;                // And set the variable as 1
+            {                            
+                digitalWrite(led, HIGH); 
+                glow = 1;                
             }
             else
-            {                           // And if LED was already on
-                digitalWrite(led, LOW); // Turn it on
-                glow = 0;               // Again set the variable as 0
+            {                           
+                digitalWrite(led, LOW); 
+                glow = 0;               
             }
         }
 
-        irrecv.resume(); // Prepare to receive another input
+        irrecv.resume(); 
     }
 }
